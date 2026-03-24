dbf9e82d8bf421b41b3cf6616a17af4736ae3d93
KamilMatejuk <kamilmatejuk@gmail.com>
2026-03-24 13:32:11 +0100

    use public urls or local svgs

--- src/components/sections/Signature.tsx
+++ src/components/sections/Signature.tsx
@@ -1,4 +1,5 @@
 import { FaFacebook, FaInstagram, FaYoutube, FaGlobe } from 'react-icons/fa';
+import SeparatorSVG from '../../assets/SeparatorSVG';
 
 const classes = {
   container: "h-52 relative gap-4 flex flex-col",
@@ -11,19 +12,18 @@ export default function Signature() {
   return (
     <div
       className={classes.container}
-      style={{ backgroundImage: `url('/src/assets/bg.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      style={{
+        backgroundImage: `url('https://chrzescijanskaszkolatanca.pl/wp-content/uploads/2023/11/Projekt-bez-nazwy.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
     >
-      <img
-        src="/src/assets/separator.svg"
-        alt="separator"
-        className={classes.separator}
-        style={{ filter: 'brightness(0) invert(1)' }}
-      />
+      <SeparatorSVG className={classes.separator} style={{ filter: 'brightness(0) invert(1)' }} />
       {/* text */}
       <div className="flex flex-col items-center text-white text-sm">
         <p>Ściskamy i pozdrawiamy!</p>
         <p>Chrześcijańska Szkoła Tańca</p>
-        <img width={60} src='/src/assets/logo.png'/>
+        <img width={60} src='https://chrzescijanskaszkolatanca.pl/wp-content/uploads/2022/04/logo_white_amen_trimmed-163x99.png' />
       </div>
       {/* icons */}
       <div className={classes.iconRow}>
